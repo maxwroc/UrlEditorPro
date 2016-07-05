7367da8a7cd2fd0435b718f9b53a4a9ca257bacb
Maks <max.chodorowski@gmail.com>
2016-07-05 15:39:41 +0100

    Populate param fields (ViewModel)

--- UrlParser/UrlParser/app/modules/view_model.js
+++ UrlParser/UrlParser/app/modules/view_model.js
@@ -4,7 +4,7 @@ var UrlParser;
         function ViewModel(url, doc) {
             var _this = this;
             this.formTextElements = ["INPUT", "TEXTAREA"];
-            this.mapIdToProperty = {
+            this.mapIdToFunction = {
                 "full_url": "url",
                 "hostname": "hostname",
                 "path": "pathname"
@@ -19,8 +19,8 @@ var UrlParser;
             var elem = evt.target;
             if (this.formTextElements.indexOf(elem.tagName) != -1) {
                 // check if element has a special property
-                if (elem.id && typeof this.mapIdToProperty[elem.id]) {
-                    this.url[this.mapIdToProperty[elem.id]](elem.value);
+                if (elem.id && typeof this.mapIdToFunction[elem.id]) {
+                    this.url[this.mapIdToFunction[elem.id]](elem.value);
                     this.populateFieldsExceptActiveOne();
                 }
             }
@@ -32,18 +32,50 @@ var UrlParser;
                 var elements = _this.doc.getElementsByTagName(tagName);
                 for (var i = 0, elem; elem = elements[i]; i++) {
                     // check if element has ID set, the mapping exists 
-                    if (elem.id && _this.mapIdToProperty[elem.id]) {
+                    if (elem.id && _this.mapIdToFunction[elem.id]) {
                         // updating element value using a function name taken from mapping
-                        _this.setValueIfNotActive(elem, _this.url[_this.mapIdToProperty[elem.id]]());
+                        _this.setValueIfNotActive(elem, _this.url[_this.mapIdToFunction[elem.id]]());
                     }
                 }
             });
+            // updating params only if there is no active element (probably this condition is redundant)
+            if (!this.doc.activeElement ||
+                // or if the active element is not one of the text fields
+                this.formTextElements.indexOf(this.doc.activeElement.tagName) == -1 ||
+                // or active element does not belong to param fields (and only if it is a full_url field)
+                // second part of this condition is an optimization as for now we have only one text field which should trigger params population
+                (!this.doc.activeElement["param-name"] && this.doc.activeElement.id == "full_url")) {
+                this.populateParams();
+            }
         };
         ViewModel.prototype.setValueIfNotActive = function (elem, value) {
             // check if it isn't currently active element (we don't want to overwrite text which user might be typing still)
             if (elem != this.doc.activeElement) {
                 elem.value = value;
             }
+        };
+        ViewModel.prototype.populateParams = function () {
+            var params = this.doc.getElementById("params");
+            // clean old set of params
+            params.innerHTML = "";
+            var urlParams = this.url.params();
+            for (var name in urlParams) {
+                var param = this.createNewParamFields(name);
+                // parameter name field
+                var paramNameElem = param.firstElementChild;
+                paramNameElem.value = name;
+                // parameter value field
+                var paramValue = paramNameElem.nextElementSibling;
+                paramValue.value = urlParams[name];
+                params.appendChild(param);
+            }
+        };
+        ViewModel.prototype.createNewParamFields = function (name) {
+            var param = document.createElement("div");
+            param.className = "param";
+            param["param-name"] = encodeURIComponent(name) || "--";
+            param.innerHTML = '<input type="text" class="name" /> <input type="text" class="value" /> <input type="checkbox" title="Encode / decode" /> <input type="button" value="x" />';
+            return param;
         };
         return ViewModel;
     })();
--- UrlParser/UrlParser/app/modules/view_model.ts
+++ UrlParser/UrlParser/app/modules/view_model.ts
@@ -6,7 +6,7 @@
 
         private formTextElements = ["INPUT", "TEXTAREA"];
 
-        private mapIdToProperty: IMap = {
+        private mapIdToFunction: IMap = {
             "full_url": "url",
             "hostname": "hostname",
             "path": "pathname"
@@ -26,8 +26,8 @@
 
             if (this.formTextElements.indexOf(elem.tagName) != -1) {
                 // check if element has a special property
-                if (elem.id && typeof this.mapIdToProperty[elem.id]) {
-                    this.url[this.mapIdToProperty[elem.id]](elem.value);
+                if (elem.id && typeof this.mapIdToFunction[elem.id]) {
+                    this.url[this.mapIdToFunction[elem.id]](elem.value);
                     this.populateFieldsExceptActiveOne();
                 }
             }
@@ -40,14 +40,22 @@
 
                 for (var i = 0, elem; elem = <HTMLInputElement>elements[i]; i++) {
                     // check if element has ID set, the mapping exists 
-                    if (elem.id && this.mapIdToProperty[elem.id]) {
+                    if (elem.id && this.mapIdToFunction[elem.id]) {
                         // updating element value using a function name taken from mapping
-                        this.setValueIfNotActive(elem, this.url[this.mapIdToProperty[elem.id]]());
+                        this.setValueIfNotActive(elem, this.url[this.mapIdToFunction[elem.id]]());
                     }
                 }
             });
-            
 
+            // updating params only if there is no active element (probably this condition is redundant)
+            if (!this.doc.activeElement ||
+                // or if the active element is not one of the text fields
+                this.formTextElements.indexOf(this.doc.activeElement.tagName) == -1 ||
+                // or active element does not belong to param fields (and only if it is a full_url field)
+                // second part of this condition is an optimization as for now we have only one text field which should trigger params population
+                (!this.doc.activeElement["param-name"] && this.doc.activeElement.id == "full_url")) {
+                this.populateParams();
+            }
         }
 
         private setValueIfNotActive(elem: HTMLInputElement, value: string) {
@@ -55,6 +63,37 @@
             if (elem != this.doc.activeElement) {
                 elem.value = value;
             }
+        }
+
+        private populateParams() {
+
+            var params = this.doc.getElementById("params");
+
+            // clean old set of params
+            params.innerHTML = "";
+
+            var urlParams = this.url.params();
+            for (var name in urlParams) {
+                var param = this.createNewParamFields(name);
+
+                // parameter name field
+                var paramNameElem = <HTMLInputElement>param.firstElementChild;
+                paramNameElem.value = name;
+
+                // parameter value field
+                var paramValue = <HTMLInputElement>paramNameElem.nextElementSibling;
+                paramValue.value = urlParams[name];
+
+                params.appendChild(param);
+            }
+        }
+
+        private createNewParamFields(name?: string): HTMLElement {
+            var param = <HTMLDivElement>document.createElement("div");
+            param.className = "param";
+            param["param-name"] = encodeURIComponent(name) || "--";
+            param.innerHTML = '<input type="text" class="name" /> <input type="text" class="value" /> <input type="checkbox" title="Encode / decode" /> <input type="button" value="x" />';
+            return param;
         }
     }
 }
