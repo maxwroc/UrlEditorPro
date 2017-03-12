cc3855a061a16f2393ab0043eccec7648605b76b
maxwroc <max.chodorowski@gmail.com>
2017-03-12 23:30:21 +0000

    Keyboard combination/shortcut to go to home (#42)
    * Resizable hostname field
    
    * Clicking on encode checkbox doesn't update the full url #33
    
    * Set focus on next param element after deleting
    
    * New blue accent color
    
    * Highlight when typing params
    
    * RichTextBox event handling refactoring + few fixes corner cases
    
    * Keyboard navigation - loop
    
    * Fix riche-textbox highlighting for paths (when short)
    
    * Autosuggest layout adjustments
    
    * Make sure that autosuggest is always visible
    
    * Disable spell check on input fields
    
    * AutoSuggest: after removing last suggestion from the list the AS box stays visible #34
    
    * Param menu (#36)
    
    * Initial menu
    
    * Move param options code to separate file
    
    * Handling events
    
    * Change func responsible for encoding
    
    * Encoding handlers - working version
    
    * Move menu to the same level as the button
    
    * Mark all new params as encoded
    
    * Move param options menu to the right position horizontally; make sure it is visible
    
    * Set url-encoded property when param is encoded
    
    * Remove encoding checkbox
    
    * Make the param options button focusable by keyboard (ctrl+right)
    
    * Adjust param menu layout
    
    * Allow only one param encoding at the time
    
    * Keyboard handling for param's menu
    
    * Auto encode if param value contains ampersand
    
    * Fix highlighting after turning on/off encoding
    
    * Fix click handling (double click event)
    
    * Move checkboxes to the right
    
    * Fix for exception in base64 decoding; Fix urldecoding
    
    * Handle escape - close menu
    
    * Param options refactoring (#38)
    
    * Initial refactoring
    
    * small changes / references / idea on handling options
    
    * Final refactored ParamOptions object
    
    * Sample usage
    
    * Added encoding options to paramoptions menu - it doesn't work perfectly
    
    * Fixed click handling for options; url encoding by default; partially fixed keyboard handling
    
    * Update to TS2.2
    
    * Fixed base64 ancoding/decoding
    
    * Additional fixes for param encoding (make sure base64 strings are as well urlEncoded)
    
    * Fix for highlighting url parts: last param was including hash
    
    * Remove redundant variables
    
    * Bump new version; Changed icon for params menu button
    
    * Add delete option to param-options menu; Fix for using obsoltete container for param-options action
    
    * Update screenshots
    
    * Small update for extension packing script
    
    * Keyboard combination/shortcut to go to home #40

---
+++ UrlEditorPRO/app/background.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="modules/url_parser.ts" />
+
+
+module UrlEditor {
+    chrome.commands.onCommand.addListener(command => {
+        if (command == "goToHomepage") {
+            chrome.tabs.getSelected(null, function (tab) {
+                var uri = new UrlEditor.Uri(tab.url);
+                chrome.tabs.update(tab.id, { url: uri.protocol() + "//" + uri.host() });
+            });
+        }
+    });
+}
