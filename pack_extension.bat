b0d7a7c278ee0f096b09a1240d97f97d9f69cb6e
maxwroc <max.chodorowski@gmail.com>
2017-05-18 22:20:46 +0100

    Updated packing script

--- pack_extension.bat
+++ pack_extension.bat
@@ -5,23 +5,19 @@ set packer="%~dp0..\WinScripts\batch\ExtensionPacker.bat"
 if "%1" neq "" set extDir=%1
 
 if not exist %extDir% (
-  set extDir="UrlEditorPRO\UrlEditorPRO\app"
+  set extDir="UrlEditorPRO\app"
 )
 
 
 if not exist %extDir% (
-  goto error
+  echo Extension directory not found:
+  echo %extDir%, %1
+  exit /b
 )
 
 if not exist %packer% (
   echo Packer not found %packer%
-  goto :eof
+  exit /b
 )
 
 %packer% %extDir%
-
-goto :eof
-
-:error
-echo Extension directory not found:
-echo %extDir%, %1
