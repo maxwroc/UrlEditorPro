40df6956775734dd11e704645dea400f70f54517
maxwroc <max.chodorowski@gmail.com>
2017-06-23 17:45:17 +0100

    Separate module for suggestions related logic on options page

--- UrlEditorPRO/app/options.ts
+++ UrlEditorPRO/app/options.ts
@@ -4,10 +4,13 @@
 
 module UrlEditor.Options {
 
-    const HOST_ALIAS_KEY = "[suggestionAlias]";
+    export interface IOnInitializedHandler {
+        (settings: Settings): void;
+    }
 
     var settings = new Settings(localStorage);
     var autoSuggestData: IAutoSuggestData;
+    let onInitializedHandlers: IOnInitializedHandler[] = [];
 
     /**
      * Automatically populates input fields if their name matches setting name.
@@ -50,10 +53,13 @@ module UrlEditor.Options {
             });
         });
 
-        if (settings.autoSuggestData) {
-            autoSuggestData = <IAutoSuggestData>JSON.parse(settings.autoSuggestData);
-            populateComboBox("autoSuggestPages", Object.keys(autoSuggestData), "-- select page --");
+        if (onInitializedHandlers) {
+            onInitializedHandlers.forEach(handler => handler(settings));
         }
+    }
+
+    export function bindOnInitializedHandler(handler: IOnInitializedHandler) {
+        onInitializedHandlers.push(handler);
     }
 
     function onChangeHandler(evt: Event): void {
@@ -84,67 +90,10 @@ module UrlEditor.Options {
                     break;
             }
         }
-        else if (elem.tagName == "SELECT") {
-            switch (elem.name) {
-                case "page":
-                    let alias = autoSuggestData[elem.value] && 
-                                autoSuggestData[elem.value][HOST_ALIAS_KEY] && 
-                                autoSuggestData[elem.value][HOST_ALIAS_KEY][0];
-                    
-                    let pageData = alias ? autoSuggestData[alias] : autoSuggestData[elem.value]
-                    populateComboBox("autoSuggestParams", Object.keys(pageData), "-- select param --", elem.value);
-
-                    let selectedIndex = 0;
-                    let defaultText = "-- select website to bind --"
-                    let filtered = Object.keys(autoSuggestData).filter((x, index) => {
-                        let areEqual = x == elem.value;
-
-                        if (x == alias) {
-                            // adding one as we add one element on the beginning of the list
-                            selectedIndex = index + 1;
-                            defaultText = "-- unbind " + alias + " --";
-                        }
-
-                        // accept if different
-                        return !areEqual;
-                    });
-                    populateComboBox("autoSuggestPagesToBind", filtered, defaultText, elem.value, selectedIndex);
-                    break;
-                case "param":
-                    var paramData = autoSuggestData[elem["source"]][elem.value] || [];
-                    var paramValuesElem = document.getElementById("autoSuggestParamValues");
-
-                    // clear param list
-                    paramValuesElem.innerHTML = "";
-
-                    paramData.forEach(value => {
-                        var paramVal = document.createElement("div");
-
-                        var input = document.createElement("input");
-                        input.type = "text";
-                        input.disabled = true;
-                        input.value = value;
-                        input.name = "paramValue";
-                        paramVal.appendChild(input);
-
-                        var deleteBtn = document.createElement("input");
-                        deleteBtn.type = "button";
-                        deleteBtn.value = "Delete";
-                        deleteBtn.name = "delete";
-                        paramVal.appendChild(deleteBtn);
-
-                        paramValuesElem.appendChild(paramVal);
-                    });
-                    break;
-            }
-        }
     }
 
     function onClickHandler(evt: MouseEvent): void {
         var elem = <HTMLInputElement>evt.target;
-        if (elem.tagName == "INPUT" && elem.name == "delete" && autoSuggestData) {
-            this.handleSuggestionDelete(elem);
-        }
 
         // general click tracking
         if (elem.getAttribute) {
@@ -152,66 +101,6 @@ module UrlEditor.Options {
             if (trackId) {
                 Tracking.trackEvent(Tracking.Category.Settings, trackId);
             }
-        }
-    }
-
-    function handleSuggestionDelete(elem: HTMLInputElement) {
-        var pageElem = <HTMLInputElement>document.getElementById("autoSuggestPages");
-        var paramElem = <HTMLInputElement>document.getElementById("autoSuggestParams");
-        var paramValues = <HTMLDivElement>document.getElementById("autoSuggestParamValues");
-
-        var saveData = false;
-
-        var subjectElem = <HTMLInputElement>elem.previousElementSibling;
-
-        // check if deleting page
-        if (subjectElem == pageElem && autoSuggestData[subjectElem.value]) {
-            if (confirm("Do you want to dletete all (" + Object.keys(autoSuggestData[subjectElem.value]).length + ") parameters for page: " + subjectElem.value)) {
-                Tracking.trackEvent(Tracking.Category.AutoSuggest, "delete page data");
-                delete autoSuggestData[subjectElem.value];
-
-                // remove element from the list
-                var select = <HTMLSelectElement><any>subjectElem;
-                select.remove(select.selectedIndex);
-                // remove all param values
-                var paramsSelect = <HTMLSelectElement><any>paramElem;
-                paramElem.innerHTML = "";
-                paramElem.value = "";
-                // remove all visible values
-                paramValues.innerHTML = "";
-
-                saveData = true;
-            }
-        }
-        // check if deleting param
-        else if (subjectElem == paramElem && autoSuggestData[pageElem.value][subjectElem.value]) {
-            if (confirm("Do you want to dletete all (" + Object.keys(autoSuggestData[pageElem.value][subjectElem.value]).length + ") values for parameter: " + subjectElem.value)) {
-                Tracking.trackEvent(Tracking.Category.AutoSuggest, "delete param data");
-                delete autoSuggestData[pageElem.value][subjectElem.value];
-
-                // remove element from the list
-                var select = <HTMLSelectElement><any>subjectElem;
-                select.remove(select.selectedIndex);
-                // remove all visible values
-                paramValues.innerHTML = "";
-
-                saveData = true;
-            }
-        }
-        // check if deleting value
-        else if (autoSuggestData[pageElem.value] &&
-            autoSuggestData[pageElem.value][paramElem.value] &&
-            autoSuggestData[pageElem.value][paramElem.value].indexOf(subjectElem.value) != -1) {
-            if (confirm("Do you want to delete '" + subjectElem.value + "' value from param '" + paramElem.value + "'")) {
-                Tracking.trackEvent(Tracking.Category.AutoSuggest, "delete param value");
-                autoSuggestData[pageElem.value][paramElem.value] = autoSuggestData[pageElem.value][paramElem.value].filter(val => val != subjectElem.value);
-                subjectElem.parentElement.parentElement.removeChild(subjectElem.parentElement);
-                saveData = true;
-            }
-        }
-
-        if (saveData) {
-            settings.setValue("autoSuggestData", JSON.stringify(autoSuggestData));
         }
     }
 
@@ -228,27 +117,6 @@ module UrlEditor.Options {
             else {
                 toggleElem.style.display = forceValue.toLowerCase() == "show" ? "block" : "none";
             }
-        }
-    }
-
-    function populateComboBox(elemId: string, data: string[], defaultValue: string = "--", comboSource: string = "", selectedIndex: number = undefined) {
-        var combo = <HTMLSelectElement>document.getElementById(elemId);
-        combo.innerHTML = "";
-        combo["source"] = comboSource;
-
-        data = data || [];
-
-        // add dummy element on the beginning
-        data.unshift(defaultValue);
-
-        data.forEach(optionValue => {
-            var option = document.createElement("option");
-            option.value = option.textContent = optionValue;
-            combo.appendChild(option);
-        });
-
-        if (selectedIndex != undefined) {
-            combo.selectedIndex = selectedIndex;
         }
     }
 
