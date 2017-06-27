41422beaf3d3d83ab167271508ee0ff63b603f87
maxwroc <max.chodorowski@gmail.com>
2017-06-27 10:41:27 +0100

    Tiny change in UT for suggestions

--- UrlEditorPRO.Tests/spec/options/suggestions.tests.ts
+++ UrlEditorPRO.Tests/spec/options/suggestions.tests.ts
@@ -1,7 +1,11 @@
-
+/// <reference path="../../../UrlEditorPro/app/modules/shared_interfaces.d.ts" />
+/// <reference path="../../../UrlEditorPro/app/modules/autosuggest.ts" />
 
 module Tests {
     describe("[Options] Suggestions - testing if ", () => {
+
+        let autoSuggestData: UrlEditor.IAutoSuggestData;
+        let initialize: Function;
 
         beforeEach(() => {
             let pageElem = createElement<HTMLSelectElement>("select", "autoSuggestPages");
@@ -12,10 +16,12 @@ module Tests {
             spyOn(UrlEditor.Helpers, "ge").and.callFake((id: string) => {
 
             });
+
+            
+            spyOn(UrlEditor.Options, "bindOnInitializedHandler").and.callFake((init: Function) => initialize = init);
         });
 
         it("ddd", () => {
-            spyOn(UrlEditor.Options, "bindOnInitializedHandler");
         });
     });
 }
