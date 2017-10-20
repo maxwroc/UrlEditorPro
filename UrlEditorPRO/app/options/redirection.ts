35b1b0fc70755c6d5beb526fb68aab164d0d59cc
Maks <max.chodorowski@gmail.com>
2017-10-20 22:04:01 +0100

    Description in options

--- UrlEditorPRO/app/options/redirection.ts
+++ UrlEditorPRO/app/options/redirection.ts
@@ -2,4 +2,15 @@
 ///<reference path="../shared/autosuggest.shared.ts" />
 
 module UrlEditor.Options.Redirection {
+    let settings: Settings;
+    
+    export function init(setts: Settings) {
+        settings = setts;
+        let redirectionsModule = Helpers.ge("redirectionsModule");
+        redirectionsModule.addEventListener("click", handleClick)
+    }
+
+    function handleClick() {
+        
+    }
 }
