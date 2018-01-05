c1f69a2807edd7c1135ed416bba726b493766b4a
maxwroc <max.chodorowski@gmail.com>
2018-01-05 22:35:27 +0000

    Background page refactoring

--- UrlEditorPRO/app/background.ts
+++ UrlEditorPRO/app/background.ts
@@ -6,94 +6,103 @@
 /// <reference path="../../typings/index.d.ts" />
 
 module UrlEditor {
-    Tracking.init(getSettings().trackingEnabled, "/background.html", false/*logEventsOnce*/);
 
-    chrome.commands.onCommand.addListener(command => {
-        switch (command) {
-            case Command.GoToHomepage:
-                Tracking.trackEvent(Tracking.Category.Redirect, "keyboard", "homepage");
-                chrome.tabs.getSelected(null, function (tab) {
-                    let uri = new UrlEditor.Uri(tab.url);
-                    chrome.tabs.update(tab.id, { url: uri.protocol() + "//" + uri.host() });
-                });
-                break;
-            case Command.RedirectUseFirstRule:
-                Tracking.trackEvent(Tracking.Category.Redirect, "keyboard", "first_rule");
-                // TODO !!!!!!!!
-                break;
+    class Background {
+        private beforeRequestListeners = [];
+        private redirMgr: RedirectionManager;
 
+        constructor(private settings = new Settings(localStorage)) {
+            Tracking.init(this.settings.trackingEnabled, "/background.html", false/*logEventsOnce*/);
         }
-    });
 
-    chrome.runtime.onMessage.addListener(msg => {
-        switch (msg) {
-            case Command.ReloadRedirectionRules:
-                // remove old listeners
-                beforeRequestListeners.forEach(l => chrome.webRequest.onBeforeRequest.removeListener(l));
-                beforeRequestListeners = [];
-                // re-initialize
-                initializeRedirections();
-                break;
-        }
-    })
-
-    let beforeRequestListeners = [];
-    function initializeRedirections() {
-        let redirect = new RedirectionManager(getSettings());
-
-        redirect.initOnBeforeRequest((urlFilter, name, handler, infoSpec) => {
-            // create new wrapper and add it to the list (we need to do it to be able to remove listener later)
-            beforeRequestListeners.push(r => {
-                let result = handler(r);
-                if (result && result.redirectUrl) {
-                    Tracking.trackEvent(Tracking.Category.Redirect, "automatic");
-                }
-
-                return result;
-            });
-            chrome.webRequest.onBeforeRequest.addListener(beforeRequestListeners[beforeRequestListeners.length -1], { urls: [urlFilter] }, infoSpec);
-        });
-    }
-
-    function initializeContextMenu(tabId: number) {
-        chrome.contextMenus.removeAll();
-
-        chrome.tabs.get(tabId, tab => {
-            let redirect = new RedirectionManager(getSettings());
-            let data = redirect.getData();
-
-            Object.keys(data).forEach(name => {
-                let rule = new RedirectRule(data[name]);
-
-                // skip all autromatic rules and ones which are not for the current url
-                if (!rule.isAutomatic && rule.isUrlSupported(tab.url)) {
-                    chrome.contextMenus.create({
-                        title: "Redirect: " + name,
-                        contexts: ["browser_action"],
-                        onclick: () => {
-                            let newUrl = rule.getUpdatedUrl(tab.url);
-                            if (tab.url != newUrl) {
-                                Tracking.trackEvent(Tracking.Category.Redirect, "click", "context_menu");
-                                chrome.tabs.update(tab.id, { url: newUrl });
-                            }
-                        }
+        public handleKeyboardCommand(command: string) {
+            switch (command) {
+                case Command.GoToHomepage:
+                    Tracking.trackEvent(Tracking.Category.Redirect, "keyboard", "homepage");
+                    chrome.tabs.getSelected(null, function (tab) {
+                        let uri = new UrlEditor.Uri(tab.url);
+                        chrome.tabs.update(tab.id, { url: uri.protocol() + "//" + uri.host() });
                     });
-                }
-            })
-        });
-    }
+                    break;
+                case Command.RedirectUseFirstRule:
+                    Tracking.trackEvent(Tracking.Category.Redirect, "keyboard", "first_rule");
 
-    function getSettings() {
-        return new Settings(localStorage);
-    }
+                    break;
 
-    chrome.tabs.onActivated.addListener(activeInfo => initializeContextMenu(activeInfo.tabId));
-    chrome.tabs.onUpdated.addListener((tabId, changedInfo) => {
-        // check if url of the current tab has changed
-        if (changedInfo.url) {
-            initializeContextMenu(tabId);
+            }
         }
-    });
 
-    initializeRedirections();
+        public handleMessage(msg: string) {
+            switch (msg) {
+                case Command.ReloadRedirectionRules:
+                    // remove old listeners
+                    this.beforeRequestListeners.forEach(l => chrome.webRequest.onBeforeRequest.removeListener(l));
+                    this.beforeRequestListeners = [];
+                    // re-initialize
+                    this.initializeRedirections();
+                    break;
+            }
+        }
+
+        public initializeRedirections() {
+            this.settings = new Settings(localStorage);
+            this.redirMgr = new RedirectionManager(this.settings);
+
+            this.redirMgr.initOnBeforeRequest((urlFilter, name, handler, infoSpec) => {
+                // create new wrapper and add it to the list (we need to do it to be able to remove listener later)
+                this.beforeRequestListeners.push(r => {
+                    let result = handler(r);
+                    if (result && result.redirectUrl) {
+                        Tracking.trackEvent(Tracking.Category.Redirect, "automatic");
+                    }
+
+                    return result;
+                });
+                chrome.webRequest.onBeforeRequest.addListener(this.beforeRequestListeners[this.beforeRequestListeners.length -1], { urls: [urlFilter] }, infoSpec);
+            });
+        }
+
+        public initializeContextMenu(tabId: number) {
+            chrome.contextMenus.removeAll();
+
+            chrome.tabs.get(tabId, tab => {
+                let data = this.redirMgr.getData();
+
+                Object.keys(data).forEach(name => {
+                    let rule = new RedirectRule(data[name]);
+
+                    // skip all autromatic rules and ones which are not for the current url
+                    if (!rule.isAutomatic && rule.isUrlSupported(tab.url)) {
+                        chrome.contextMenus.create({
+                            title: "Redirect: " + name,
+                            contexts: ["browser_action"],
+                            onclick: () => {
+                                let newUrl = rule.getUpdatedUrl(tab.url);
+                                if (tab.url != newUrl) {
+                                    Tracking.trackEvent(Tracking.Category.Redirect, "click", "context_menu");
+                                    chrome.tabs.update(tab.id, { url: newUrl });
+                                }
+                            }
+                        });
+                    }
+                })
+            });
+        }
+
+        public handleTabUpdate(tabId: number, changedInfo: chrome.tabs.TabChangeInfo) {
+            // check if url of the current tab has changed
+            if (changedInfo.url) {
+                this.initializeContextMenu(tabId);
+            }
+        }
+    }
+
+    const bg = new Background();
+
+    chrome.commands.onCommand.addListener(cmd => bg.handleKeyboardCommand(cmd));
+    chrome.runtime.onMessage.addListener(msg => bg.handleMessage(msg));
+    chrome.tabs.onActivated.addListener(activeInfo => bg.initializeContextMenu(activeInfo.tabId));
+    chrome.tabs.onUpdated.addListener((tabId, changedInfo) => bg.handleTabUpdate(tabId, changedInfo));
+
+    bg.initializeRedirections();
 }
